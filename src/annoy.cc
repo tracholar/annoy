e3504d9b3e2fdac9ddf31a8ee5beb7fe4e329172
zuoyuan <zuoyuan@meituan.com>
2019-08-20 22:07:33 +0800

    add hive ann

---
+++ src/annoy.cc
@@ -0,0 +1,115 @@
+// Copyright (c) 2013 Spotify AB
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+// Author zuoyuan@mail.ustc.edu.cn 
+// annoy index for hive transform 
+
+#include<stddef.h>
+#include "kissrandom.h"
+#include "annoylib.h"
+#define MODE_INDEX 1 
+#define MODE_SEARCH 2
+
+int ArgPos(char *str, int argc, char **argv) {
+  int a;
+  for (a = 1; a < argc; a++) if (!strcmp(str, argv[a])) {
+    if (a == argc - 1) {
+      printf("Argument missing for %s\n", str);
+      exit(1);
+    }
+    return a;
+  }
+  return -1;
+}
+
+int main(int argc, char **argv){
+  int i;
+  if (argc == 1) {
+    printf("ANN search toolkit v 0.1c\n\n");
+    printf("Options:\n");
+    
+    return 0;
+  }
+
+  int size = 0;
+  int nnsize = 100;
+  int tree = 10;
+  int mode = MODE_INDEX;
+  char input[256] = {0};
+  char output[256] = {0};
+  if ((i = ArgPos((char *)"-size", argc, argv)) > 0) size = atoi(argv[i + 1]);
+  if ((i = ArgPos((char *)"-nnsize", argc, argv)) > 0) nnsize = atoi(argv[i + 1]);
+  if ((i = ArgPos((char *)"-tree", argc, argv)) > 0) tree = atoi(argv[i + 1]);
+  if ((i = ArgPos((char *)"-mode", argc, argv)) > 0) mode = atoi(argv[i + 1]);
+  if ((i = ArgPos((char *)"-input", argc, argv)) > 0) strncpy(input, argv[i+1], 256);
+  if ((i = ArgPos((char *)"-output", argc, argv)) > 0) strncpy(output, argv[i+1], 256);
+
+  FILE * fin = stdin;
+  FILE * fout = stdout;
+  if(input[0] != 0){
+      fin = fopen(input, "r");
+      if(fin == NULL){
+          fprintf(stderr, "ERROR: open file %s failed!\n", input);
+          exit(1);
+      }
+  }
+  if(output[0] != 0){
+      fin = fopen(output, "w");
+      if(fin == NULL){
+          fprintf(stderr, "ERROR: open file %s failed!\n", output);
+          exit(1);
+      }
+  }
+  
+
+  AnnoyIndex<long long, float, Angular, Kiss32Random> t = AnnoyIndex<long long, float, Angular, Kiss32Random>(size);
+
+  // read data 
+  float * buff = new float[size];
+  long long n = 0;
+  long long key = 0;
+  std::vector<long long> keys;
+  while(!feof(fin)){
+    scanf("%lld", &key);
+    keys.push_back(key);
+    for(int i=0; i<size; i++){
+        scanf("%f", &buff[i]);
+    }
+    t.add_item(key, buff);
+    n++;
+  }
+  t.build(tree);
+  
+  std::vector<long long> closest;
+  std::vector<float> distances;
+  
+  for(long long i=0; i<n; i++){
+      t.get_nns_by_item(keys[i], nnsize, n, &closest, &distances);
+      fprintf(fout, "%lld\t", keys[i]);
+      for(int j=0; j<closest.size(); j++){
+          fprintf(fout, "%lld ", closest[j]);
+      }
+        
+      fputc( '\t', fout);
+      for(int j=0; j<distances.size(); j++){
+          fprintf(fout, "%g ", distances[j]);
+      }
+      fputc('\n', fout);
+
+      closest.clear(); 
+      distances.clear();
+  }
+
+  delete[] buff;
+}
